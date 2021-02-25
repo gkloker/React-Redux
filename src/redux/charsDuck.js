4d5913caabee575e5f6a8398df4b2e26e202e224
Gabriel Kloker <>
2021-02-25 20:35:31 -0300

    Redux with React hooks

---
+++ src/redux/charsDuck.js
@@ -0,0 +1,75 @@
+import {
+  GET_CHARACTERS,
+  GET_CHARACTERS_SUCCESS,
+  GET_CHARACTERS_ERROR,
+  REMOVE_CHARACTER,
+} from '../types';
+import axios from 'axios';
+// require('dotenv').config({ path: '.env' });
+
+// Constants
+let initialData = {
+  fetching: false,
+  array: [],
+  current: {}
+}
+let URL = "https://rickandmortyapi.com/api/character";
+
+// Reducer
+export default function reducer(state = initialData, action) {
+  switch(action.type) {
+    case GET_CHARACTERS:
+      return {
+        ...state,
+        fetching: true
+      }
+    case GET_CHARACTERS_SUCCESS:
+      return {
+        ...state,
+        array: action.payload,
+        fetching: false
+      }
+    case GET_CHARACTERS_ERROR:
+      return {
+        ...state,
+        error: action.payload,
+        fetching: false
+      }
+    case REMOVE_CHARACTER:
+      return {
+        ...state,
+        array: action.payload
+      }
+    default:
+      return state
+  }
+}
+
+// Action (action creator, thunk)
+export let getCharactersAction = () => (dispatch, getState) => {
+  dispatch({
+    type: GET_CHARACTERS
+  });
+  return axios.get(URL)
+    .then(res => {
+      dispatch({
+        type: GET_CHARACTERS_SUCCESS,
+        payload: res.data.results
+      })
+    })
+    .catch(error => {
+      dispatch({
+        type: GET_CHARACTERS_ERROR,
+        payload: error.request.response
+      });
+    });
+}
+
+export let removeCharacterAction = () => (dispatch, getState) => {
+  let { array } = getState().characters;
+  array.shift();
+  dispatch({
+    type: REMOVE_CHARACTER,
+    payload: [...array]
+  });
+}
---
+++ src/redux/userDuck.js
@@ -0,0 +1,15 @@
+// Constants
+let initialData = {
+  loggedIn: false
+}
+// let LOGIN = "LOGIN"
+
+// Reducer
+export default function reducer(state = initialData, action) {
+  switch(action.type) {
+    default:
+      return state
+  }
+}
+
+// Action (action creator)
